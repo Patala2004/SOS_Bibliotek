45be77c5c2614fe4290ce47e6df28be879b1fda3
Christian <christian.most@alumnos.upm.es>
2025-04-27 17:21:31 +0200

    Añadido pagina de info basica del usuario

--- src/main/java/es/upm/es/Libreria/controller/UserController.java
+++ src/main/java/es/upm/es/Libreria/controller/UserController.java
@@ -84,22 +84,17 @@ public class UserController {
             historial.add(EntityModel.of(prestamo.getLibro(), linkTo(methodOn(LibroController.class).getLibro(prestamo.getLibro().getId())).withSelfRel()));
             counterOnlyFive--;
         }
-        // for(UserLibro userLibroEntity : userLibroService.buscarPorUserId(id)){
-        //     UserLibroDTO userLibroDTO = new UserLibroDTO(userLibroEntity.getId(), userLibroEntity.getLibro(), userLibroEntity.getFechaInicio(), userLibroEntity.getFechaFin(), userLibroEntity.getFechaDevolucion());
-        //     if(userLibroDTO.getLibro().getLinks().isEmpty()){
-        //         userLibroDTO.getLibro().add(linkTo(methodOn(LibroController.class).getLibro(userLibroEntity.getLibro().getId())).withSelfRel());
-        //     }
-            
-        //     if(userLibroEntity.getFechaDevolucion() != null){
-        //         historial.add(EntityModel.of(userLibroDTO, linkTo(methodOn(UserLibroController.class).getPrestamo(id)).withSelfRel()));
-        //     }
-        //     else{
-        //         prestamos.add(EntityModel.of(userLibroDTO, linkTo(methodOn(UserLibroController.class).getPrestamo(id)).withSelfRel()));
-        //     }
-        // }
-
         user.setPrestamos(prestamos);
         user.setHistorial(historial);
+        user.add(linkTo(methodOn(UserController.class).getUser(id)).withSelfRel());
+        return ResponseEntity.ok(user);
+    }
+
+    // Get user by id and show only basic info
+    @GetMapping(value = "{id}/basico", produces = {"application/json", "application/xml"})
+    public ResponseEntity<User> getUserBasicInfo(@PathVariable Integer id){
+        User user = service.buscarPorId(id)
+        .orElseThrow(() -> new UserNotFoundException(id));
         user.add(linkTo(methodOn(UserController.class).getUser(id)).withSelfRel());
         return ResponseEntity.ok(user);
     }
